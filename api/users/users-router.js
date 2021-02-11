cd73524282c23e66fcf3d8a7861c29cca1e1377c
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-10 19:45:47 -0800

    only need one more user router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -13,30 +13,86 @@ router.get('/', (req, res) => {
   })
 });
 
+
+
+
 router.get('/:id', mw.validateUserId,(req, res) => {
   res.status(200).json(req.user)
 });
 
+
+
 router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+  .then((newUser) => {
+    if(newUser){
+      res.status(201).json(`New user successfully added`)
+    }else{
+      res.status(400).json({message: `Please provide username`})
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({message: `Server error: ${err}, could not post`})
+  })
+
 });
+
+
 
 router.put('/:id', mw.validateUserId, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+  const id = req.params.id
+  const changes = req.body
+
+  Users.update(id,changes)
+    .then((updatedUser) => {
+      if(!id){
+        res.status(404).json({message:`The post with specified ID: ${id} does not exist`})
+      }else if(!changes){
+        res.status(400).json({message: `Please make a change`})
+      }else{
+        res.status(200).json(updatedUser)
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'User could not be updated' });
+    });
 });
+
+
 
 router.delete('/:id',mw.validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+    const id = req.params.id
+    Users.remove(id)
+    .then((user) => {
+      if(user){
+        res.status(200).json({message: `User with ID: ${id} was successfully destroyed`})
+      }else{
+        res.status(404).json({message: `User with ID: ${id} does not exist`})
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({message: `Server error: ${err}, user could not be destroyed`})
+    })
 });
 
+
+
 router.get('/:id/posts',mw.validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+  const id = req.params.id
+  Users.getUserPosts()
+  .then((post) =>{
+    if(post.length > 0 ){
+      res.status(200).json(post)
+    }else{
+      res.status(404).json({message:`Post with specified ID: ${id} does note exist`})
+    }
+  })
+  .catch( 
+    res.status(500).json({message: `Posts could not be received`})
+    )
 });
+
+
 
 router.post('/:id/posts', mw.validateUserId,(req, res) => {
   // RETURN THE NEWLY CREATED USER POST
